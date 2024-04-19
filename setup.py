52f12b82f6037f84cf53fabd2376717233e66521
Ivan D'Onofrio <d.onofrio.ivan@gmail.com>
2024-04-20 00:48:02 +0200

    Now the package can be installed

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='temporal_graph_learning',
+    version='0.0.1',
+    packages=find_packages(include=['src', 'src.*']),
+    install_requires=[],
+)
--- src/charts/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import Histogram
-from . import Lineplot
-from . import Plot
-from . import Plots
