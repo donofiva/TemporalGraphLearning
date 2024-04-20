9971d227a940a1c8bfbd2324089d0a5874f27eed
Ivan D'Onofrio <d.onofrio.ivan@gmail.com>
2024-04-20 13:59:06 +0200

    Added directory creation methods

--- src/temporal_graph_learning/io/FileManager.py
+++ src/temporal_graph_learning/io/FileManager.py
@@ -1,20 +1,8 @@
 import os
 
-from pathlib import Path
-from src.io.Directory import Directory
-from src.io.File import File
-
 
 class FileManager:
 
     @staticmethod
-    def get_cwd() -> Path:
-        return Path(os.getcwd()).resolve()
-
-    @staticmethod
-    def resolve_directory(*directories: Directory) -> Path:
-        return FileManager.get_cwd().joinpath(*[directory.value for directory in directories])
-
-    @staticmethod
-    def resolve_file(directory: Path, file: File) -> Path:
-        return directory.joinpath(file.value)
+    def directory_create(path: str):
+        os.makedirs(path, exist_ok=True)
