b9cb6eef759be1913160f5af5e8a8cf1efd57cf7
Ivan D'Onofrio <d.onofrio.ivan@gmail.com>
2024-04-20 13:30:36 +0200

    Added Dataset abstraction

---
+++ src/temporal_graph_learning/abstraction/Dataset.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+from typing import List, Tuple, Any
+
+
+class Dataset:
+
+    def __init__(self, dataframe: pd.DataFrame):
+        self._dataframe = dataframe
+
+    def slice_dataset(self,  dimensions: List[str], include_all: bool = False) -> List[Tuple[Any, pd.DataFrame]]:
+        return (
+            list(self._dataframe.groupby(dimensions, as_index=False)) +
+            ([] if not include_all else [('ALL', self._dataframe)])
+        )
