7e00e2d33ce84beaf25edbeaef08df7c29e09585
Ivan D'Onofrio <d.onofrio.ivan@gmail.com>
2024-04-20 13:32:28 +0200

    Improved Dataset slice method

--- src/temporal_graph_learning/abstraction/Dataset.py
+++ src/temporal_graph_learning/abstraction/Dataset.py
@@ -8,7 +8,7 @@ class Dataset:
     def __init__(self, dataframe: pd.DataFrame):
         self._dataframe = dataframe
 
-    def slice_dataset(self,  dimensions: List[str], include_all: bool = False) -> List[Tuple[Any, pd.DataFrame]]:
+    def slice_on_dimensions(self,  dimensions: List[str], include_all: bool = False) -> List[Tuple[Any, pd.DataFrame]]:
         return (
             list(self._dataframe.groupby(dimensions, as_index=False)) +
             ([] if not include_all else [('ALL', self._dataframe)])
