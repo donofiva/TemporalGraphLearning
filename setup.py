919ed409182dec5284259cf6da6f75555bbfe960
Ivan D'Onofrio <d.onofrio.ivan@gmail.com>
2024-04-20 02:12:55 +0200

    Now the package can be installed

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='temporal_graph_learning',
     version='0.0.1',
-    package_dir={'temporal_graph_learning': 'src'},
-    packages=find_packages(where=['src', 'src*']),
+    package_dir={'': 'src'},
+    packages=['temporal_graph_learning'],
     install_requires=[
         'pandas'
     ],
