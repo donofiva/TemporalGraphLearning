3d2909d0ebd00e1b33dec7ccacbd45d5c68760a1
Ivan D'Onofrio <d.onofrio.ivan@gmail.com>
2024-04-20 16:52:20 +0200

    Added directory creation methods

--- src/temporal_graph_learning/io/FileManager.py
+++ src/temporal_graph_learning/io/FileManager.py
@@ -6,3 +6,7 @@ class FileManager:
     @staticmethod
     def directory_create(path: str):
         os.makedirs(path, exist_ok=True)
+
+    @staticmethod
+    def file_resolve_file_path(directory: str, file: str):
+        return '/'.join([directory, file])
