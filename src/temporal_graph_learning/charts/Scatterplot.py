b6dc5234f8f96b5ed9abe8c16c8a5b27bdd154d5
Ivan D'Onofrio <d.onofrio.ivan@gmail.com>
2024-04-21 19:17:50 +0200

    Added scatterplot abstraction

---
+++ src/temporal_graph_learning/charts/Scatterplot.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from typing import Union, List
+from temporal_graph_learning.charts.Plot import Plot
+
+
+class Scatterplot:
+
+    def __init__(self, opacity: float):
+
+        # Store heatmap configuration
+        self._opacity = max(1.0, opacity)
+
+    def draw_on_plot(
+            self,
+            datapoints_x: Union[pd.Series, List],
+            datapoints_y: Union[pd.Series, List],
+            plot: Plot
+    ):
+
+        # Convert datapoints to NumPy array
+        datapoints_x = np.array(datapoints_x)
+        datapoints_y = np.array(datapoints_y)
+
+        # Draw scatterplot
+        plot.get_pointer().scatter(datapoints_x, datapoints_y, alpha=self._opacity)
