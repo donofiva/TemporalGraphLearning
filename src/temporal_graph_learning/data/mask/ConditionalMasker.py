7ae3e41d7381b67c68c02e9ccc166ef9d01093c4
Ivan D'Onofrio <d.onofrio.ivan@gmail.com>
2024-04-26 00:38:28 +0200

    Added ConditionalMasker for data cleaning

---
+++ src/temporal_graph_learning/data/mask/ConditionalMasker.py
@@ -0,0 +1,85 @@
+import numpy as np
+import pandas as pd
+
+from enum import Enum
+from dataclasses import dataclass
+from typing import Any, List, Union
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class MaskingComparisonType(Enum):
+
+    GREATER = 0
+    LESS = 1
+    EQUAL = 2
+    NOT_EQUAL = 3
+    IN = 4
+    NOT_IN = 5
+
+    def compare(self, target, value):
+        return {
+            MaskingComparisonType.GREATER: lambda x, y: x > y,
+            MaskingComparisonType.LESS: lambda x, y: x < y,
+            MaskingComparisonType.EQUAL: lambda x, y: x == y,
+            MaskingComparisonType.NOT_EQUAL: lambda x, y: x != y,
+            MaskingComparisonType.IN: lambda x, y: x.isin(y),
+            MaskingComparisonType.NOT_IN: lambda x, y: ~x.isin(y),
+        }[self](target, value)
+
+
+@dataclass
+class MaskingComparison:
+
+    dimension: str
+    type: MaskingComparisonType
+    value: Any
+
+
+@dataclass
+class MaskingCondition:
+
+    dimensions: Union[str, List[str]]
+    comparisons: List[MaskingComparison]
+
+
+class ConditionalMasker(BaseEstimator, TransformerMixin):
+
+    def __init__(self, masking_conditions: List[MaskingCondition] = None):
+        self._masking_conditions = masking_conditions or set()
+
+    @staticmethod
+    def _parse_masking_condition_dimensions(X, dimensions: Union[str, List[str]]):
+
+        # Select all the dimensions on __ALL__ wildcard
+        if dimensions == "__ALL__":
+            return X.columns
+
+        # Otherwise return dimensions
+        return dimensions
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+
+        # Make sure that the input is a Pandas DataFrame
+        if not isinstance(X, pd.DataFrame):
+            raise TypeError("Input must be a pandas DataFrame.")
+
+        # Create a copy of the DataFrame to avoid modifying the original data
+        X = X.copy()
+
+        # Apply masking conditions iteratively
+        for condition in self._masking_conditions:
+
+            # Create a mask for all comparisons related to the current condition
+            conditional_mask = pd.Series([False] * X.shape[0], index=X.index)
+
+            # Evaluate all comparisons related to the current condition and accumulate masking conditions
+            for comparison in condition.comparisons:
+                conditional_mask |= comparison.type.compare(X[comparison.dimension], comparison.value)
+
+            # Apply the combined mask to the target column
+            X.loc[conditional_mask, self._parse_masking_condition_dimensions(X, condition.dimensions)] = np.nan
+
+        return X
---
+++ test/data/mask/test_ConditionalMasker.py
@@ -0,0 +1,101 @@
+import unittest
+from temporal_graph_learning.data.mask.ConditionalMasker import *
+
+
+class TestConditionalMasker(unittest.TestCase):
+
+    def setUp(self):
+        self.data = pd.DataFrame({
+            'A': [1, 2, 3, 4, 5],
+            'B': [5, 4, 3, 2, 1],
+            'C': [10, 20, 30, 40, 50]
+        })
+        # Define conditions using Lists
+        self.masker = ConditionalMasker([
+            MaskingCondition(
+                dimensions='A',
+                comparisons=[MaskingComparison('B', MaskingComparisonType.LESS, 3)]
+            )
+        ])
+
+    def test_mask_greater_than(self):
+        masker = ConditionalMasker([
+            MaskingCondition(
+                dimensions='A',
+                comparisons=[MaskingComparison('C', MaskingComparisonType.GREATER, 25)]
+            )
+        ])
+        result = masker.transform(self.data.copy())
+        expected = pd.Series([1, 2, np.nan, np.nan, np.nan], name='A')
+        pd.testing.assert_series_equal(result['A'], expected)
+
+    def test_mask_less_than(self):
+        masker = ConditionalMasker([
+            MaskingCondition(
+                dimensions='B',
+                comparisons=[MaskingComparison('C', MaskingComparisonType.LESS, 25)]
+            )
+        ])
+        result = masker.transform(self.data.copy())
+        expected = pd.Series([np.nan, np.nan, 3, 2, 1], name='B')
+        pd.testing.assert_series_equal(result['B'], expected)
+
+    def test_mask_equal_to(self):
+        masker = ConditionalMasker([
+            MaskingCondition(
+                dimensions='B',
+                comparisons=[MaskingComparison('A', MaskingComparisonType.EQUAL, 2)]
+            )
+        ])
+        result = masker.transform(self.data.copy())
+        expected = pd.Series([5, np.nan, 3, 2, 1], name='B')
+        pd.testing.assert_series_equal(result['B'], expected)
+
+    def test_mask_not_equal_to(self):
+        masker = ConditionalMasker([
+            MaskingCondition(
+                dimensions='B',
+                comparisons=[MaskingComparison('A', MaskingComparisonType.NOT_EQUAL, 3)]
+            )
+        ])
+        result = masker.transform(self.data.copy())
+        expected = pd.Series([np.nan, np.nan, 3, np.nan, np.nan], name='B')
+        pd.testing.assert_series_equal(result['B'], expected)
+
+    def test_mask_in(self):
+        masker = ConditionalMasker([
+            MaskingCondition(
+                dimensions='A',
+                comparisons=[MaskingComparison('A', MaskingComparisonType.IN, {2, 4})]
+            )
+        ])
+        result = masker.transform(self.data.copy())
+        expected = pd.Series([1, np.nan, 3, np.nan, 5], name='A')
+        pd.testing.assert_series_equal(result['A'], expected)
+
+    def test_mask_not_in(self):
+        masker = ConditionalMasker([
+            MaskingCondition(
+                dimensions='A',
+                comparisons=[MaskingComparison('A', MaskingComparisonType.NOT_IN, {2, 4})]
+            )
+        ])
+        result = masker.transform(self.data.copy())
+        expected = pd.Series([np.nan, 2, np.nan, 4, np.nan], name='A')
+        pd.testing.assert_series_equal(result['A'], expected)
+
+    def test_input_validation(self):
+        masker = ConditionalMasker([
+            MaskingCondition(
+                dimensions='A',
+                comparisons=[MaskingComparison('C', MaskingComparisonType.GREATER, 25)]
+            )
+        ])
+        with self.assertRaises(TypeError):
+            masker.transform(np.array([1, 2, 3]))
+
+    def test_no_conditions(self):
+        masker = ConditionalMasker([])
+        result = masker.transform(self.data.copy())
+        pd.testing.assert_frame_equal(result, self.data)
+
