da19205f5c2a1b9b213242f824e67e166fc24b1d
Ivan D'Onofrio <d.onofrio.ivan@gmail.com>
2024-08-23 00:03:59 +0200

    Updated WindFarmEstimator

--- src/temporal_graph_learning/models/ensemble/WindFarmEstimator.py
+++ src/temporal_graph_learning/models/ensemble/WindFarmEstimator.py
@@ -1,48 +1,144 @@
+import copy
+import numpy as np
 import pandas as pd
 
-from typing import Tuple, Dict
+from tqdm import tqdm
+from typing import List, Dict
+from sklearn.model_selection import train_test_split
 
 
 class WindFarmEstimator:
 
-    def __init__(self, wind_turbine_to_estimator: Dict, apply_mask: bool = True):
-        self._wind_turbine_to_estimator = wind_turbine_to_estimator
-        self._apply_mask = apply_mask
+    def __init__(
+            self,
+            channels: pd.DataFrame,
+            masks: pd.DataFrame,
+            targets: pd.DataFrame, estimator,
+            test_size: float = 0.2,
+            shuffle_dataset: bool = False
+    ):
 
-    def predict(self, wind_turbine_to_X_mask: Dict[int, Tuple[pd.DataFrame, pd.DataFrame]]):
+        # Channels, masks and targets
+        self._channels = channels
+        self._masks = masks
+        self._targets = targets
 
-        # Predictions buffer
-        y_wind_farm = []
+        # Estimator template
+        self._estimator = estimator
 
-        # Iterate over wind turbine in wind farm
-        for wind_turbine, estimator in self._wind_turbine_to_estimator.items():
+        # Train test split configuration
+        self._test_size = test_size
+        self._shuffle_dataset = shuffle_dataset
 
-            # Retrieve estimator
-            X_wind_turbine, mask_wind_turbine = wind_turbine_to_X_mask[wind_turbine]
+        # Train and test split
+        self._channels_train = None
+        self._channels_test = None
+        self._masks_train = None
+        self._masks_test = None
+        self._targets_train = None
+        self._targets_test = None
 
-            # Predict y and store it as a dataframe
-            y_wind_turbine = estimator.predict(X_wind_turbine)
-            y_wind_turbine = pd.DataFrame(
-                y_wind_turbine,
-                columns=mask_wind_turbine.columns,
-                index=mask_wind_turbine.index
-            )
+        # Wind turbines split
+        self._wind_turbines: List[int] = []
+        self._wind_turbine_to_estimator: Dict = {}
 
-            # Apply mask, if required
-            if self._apply_mask:
+    def _get_estimator(self):
+        return copy.deepcopy(self._estimator)
 
-                # Preserve mask
-                mask_wind_turbine = mask_wind_turbine.copy()
-                mask_wind_turbine.columns = y_wind_turbine.columns
+    def _load_train_test_split(self):
+        (
+            self._channels_train,
+            self._channels_test,
+            self._masks_train,
+            self._masks_test,
+            self._targets_train,
+            self._targets_test
+        ) = train_test_split(
+            self._channels,
+            self._masks,
+            self._targets,
+            test_size=self._test_size,
+            shuffle=self._shuffle_dataset
+        )
 
-                # Apply mask
-                y_wind_turbine = y_wind_turbine.where(mask_wind_turbine, 0.0)
+    def _load_wind_turbines(self):
+        self._wind_turbines = self._channels.columns.get_level_values(0).unique().tolist()
 
-            # Store prediction
-            y_wind_farm.append(y_wind_turbine)
+    def _load_estimators(self):
+        self._wind_turbine_to_estimator = {
+            wind_turbine: self._get_estimator()
+            for wind_turbine in self._wind_turbines
+        }
 
-        # Produce resulting dataset
-        y_wind_farm = pd.concat(y_wind_farm, axis=0)
-        y_wind_farm = y_wind_farm.groupby(y_wind_farm.index).sum()
+    def initialize(self):
 
-        return y_wind_farm
+        # Load wind turbines and estimators
+        self._load_wind_turbines()
+        self._load_estimators()
+
+        # Split dataset
+        self._load_train_test_split()
+
+    def fit(self):
+
+        # Fit model for each wind turbine
+        for wind_turbine in tqdm(self._wind_turbines):
+            # Retrieve estimator and dataset slices on wind turbine
+            estimator = self.get_estimator_by_wind_turbine(wind_turbine)
+            channels, _, targets = self.get_train_channels_masks_and_targets_by_wind_turbine(wind_turbine)
+
+            # Train estimator
+            estimator.fit(channels, targets)
+
+    def predict(self) -> pd.DataFrame:
+
+        # Prediction buffer
+        predictions = []
+
+        # Fit model for each wind turbine
+        for wind_turbine in tqdm(self._wind_turbines):
+            # Retrieve estimator and dataset slices on wind turbine
+            estimator = self.get_estimator_by_wind_turbine(wind_turbine)
+            channels, _, _ = self.get_train_channels_masks_and_targets_by_wind_turbine(wind_turbine)
+
+            # TODO: Add here masking logic
+
+            # Train estimator
+            prediction = estimator.predict(channels)
+            predictions.append(prediction)
+
+        return pd.DataFrame(
+            np.hstack(predictions),
+            columns=self._targets_test.columns
+        )
+
+    def get_train_channels_masks_and_targets(self):
+        return (
+            self._channels_train,
+            self._masks_train,
+            self._targets_train
+        )
+
+    def get_test_channels_masks_and_targets(self):
+        return (
+            self._channels_test,
+            self._masks_test,
+            self._targets_test
+        )
+
+    def get_train_channels_masks_and_targets_by_wind_turbine(self, wind_turbine: int):
+        return (
+            self._channels_train.loc[:, pd.IndexSlice[wind_turbine, :]],
+            self._masks_train.loc[:, pd.IndexSlice[wind_turbine, :]],
+            self._targets_train.loc[:, pd.IndexSlice[wind_turbine, :]],
+        )
+
+    def get_test_channels_masks_and_targets_by_wind_turbine(self, wind_turbine: int):
+        return (
+            self._channels_test.loc[:, pd.IndexSlice[wind_turbine, :]],
+            self._masks_test.loc[:, pd.IndexSlice[wind_turbine, :]],
+            self._targets_test.loc[:, pd.IndexSlice[wind_turbine, :]],
+        )
+
+    def get_estimator_by_wind_turbine(self, wind_turbine: int):
+        return self._wind_turbine_to_estimator.get(wind_turbine)
