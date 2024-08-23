749e7cdf9b9eaa01b4ca23164566bdec55b10d3b
Ivan D'Onofrio <d.onofrio.ivan@gmail.com>
2024-08-23 20:47:35 +0200

    Added masks to LagLeadDatasetPreprocessor

--- src/temporal_graph_learning/data/preprocessing/LagLeadDatasetPreprocessor.py
+++ src/temporal_graph_learning/data/preprocessing/LagLeadDatasetPreprocessor.py
@@ -38,55 +38,57 @@ class LagLeadDatasetPreprocessor:
         # Concatenate all shifted dataframes
         return pd.concat(shifted_dfs, axis=1)
 
-    def _lag_features(self, X: pd.DataFrame):
+    def _lag_features(self, channels: pd.DataFrame):
 
         # Define shifts and suffixes for lagging
         shifts = list(range(self.window - 1, -1, -1))
         suffixes = [f"W{lag}" for lag in shifts]
 
         # Handle multi-index columns by grouping at the first level
-        if isinstance(X.columns, pd.MultiIndex):
+        if isinstance(channels.columns, pd.MultiIndex):
 
-            lagged_X_slices = [
+            lagged_channels_slices = [
                 self._shift_and_rename(group, shifts, suffixes)
-                for _, group in X.groupby(level=0, axis=1)
+                for _, group in channels.groupby(level=0, axis=1)
             ]
 
-            return pd.concat(lagged_X_slices, axis=1)
+            return pd.concat(lagged_channels_slices, axis=1)
 
         # Apply lagging directly for single-level columns
-        return self._shift_and_rename(X, shifts, suffixes)
+        return self._shift_and_rename(channels, shifts, suffixes)
 
-    def _lead_targets(self, y: pd.DataFrame):
+    def _lead_targets(self, targets: pd.DataFrame):
 
         # Define shifts and suffixes for leading
         shifts = [-(self.delay + lead) for lead in range(self.horizon)]
         suffixes = [f"D{self.delay}_H{lead}" for lead in range(self.horizon)]
 
         # Handle multi-index columns by grouping at the first level
-        if isinstance(y.columns, pd.MultiIndex):
+        if isinstance(targets.columns, pd.MultiIndex):
 
-            lead_y_slices = [
+            lead_targets_slices = [
                 self._shift_and_rename(group, shifts,  suffixes)
-                for _, group in y.groupby(level=0, axis=1)
+                for _, group in targets.groupby(level=0, axis=1)
             ]
 
-            return pd.concat(lead_y_slices, axis=1)
+            return pd.concat(lead_targets_slices, axis=1)
 
         # Apply leading directly for single-level columns
-        return self._shift_and_rename(y, shifts,  suffixes)
+        return self._shift_and_rename(targets, shifts,  suffixes)
 
-    def transform(self, X: pd.DataFrame, y: pd.DataFrame):
+    def transform(self, channels: pd.DataFrame, masks: pd.DataFrame, targets: pd.DataFrame):
 
         # Create lagged features and lead targets
-        lagged_X = self._lag_features(X)
-        lead_y = self._lead_targets(y)
+        lagged_channels = self._lag_features(channels)
+        lead_masks = self._lead_targets(masks)
+        lead_targets = self._lead_targets(targets)
 
         # Align the indices by dropping rows with NaNs from both dataframes
-        valid_indices = lagged_X.dropna().index.intersection(lead_y.dropna().index)
+        valid_indices = lagged_channels.dropna().index.intersection(lead_targets.dropna().index)
 
         # Filter datasets by valid indices
-        lagged_X = lagged_X.loc[valid_indices]
-        lead_y = lead_y.loc[valid_indices]
+        lagged_channels = lagged_channels.loc[valid_indices]
+        lead_masks = lead_masks.loc[valid_indices]
+        lead_targets = lead_targets.loc[valid_indices]
 
-        return lagged_X, lead_y
+        return lagged_channels, lead_masks, lead_targets
