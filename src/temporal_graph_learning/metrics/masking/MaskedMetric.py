0f996616e994798fcc87fcc1edbf98c3452dad80
Ivan D'Onofrio <d.onofrio.ivan@gmail.com>
2024-08-23 23:23:26 +0200

    Improved masking logic

--- src/temporal_graph_learning/metrics/masking/MaskedMetric.py
+++ src/temporal_graph_learning/metrics/masking/MaskedMetric.py
@@ -3,14 +3,15 @@ import pandas as pd
 
 class MaskedMetric:
 
-    def __init__(self, metric):
-        self._metric = metric
+    def __init__(self, metric, masks: pd.DataFrame):
 
-    @staticmethod
-    def _mask_targets(targets: pd.DataFrame, masks: pd.DataFrame):
+        self._metric = metric
+        self._masks = masks
+
+    def _mask_targets(self, targets: pd.DataFrame):
 
         # Apply mask
-        targets_masked = targets.values * masks.values
+        targets_masked = targets.values * self._masks.values
 
         # Rebuild targets
         return pd.DataFrame(
@@ -19,8 +20,8 @@ class MaskedMetric:
             columns=targets.columns
         )
 
-    def compute(self, targets: pd.DataFrame, targets_predicted: pd.DataFrame, masks: pd.DataFrame):
+    def compute(self, targets: pd.DataFrame, targets_predicted: pd.DataFrame):
         return self._metric.compute(
-            self._mask_targets(targets, masks),
-            self._mask_targets(targets_predicted, masks)
+            self._mask_targets(targets),
+            self._mask_targets(targets_predicted)
         )
